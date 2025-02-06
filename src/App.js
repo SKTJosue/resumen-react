96801d088a895f39432ca5d4a9f978bdb7a2d15c
SKTJosue <144604072+SKTJosue@users.noreply.github.com>
2025-02-06 01:40:52 +0000

    first commmit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { Presentacion } from './component/Presentacion';
+
+function App() {
+  return (
+      <Presentacion></Presentacion>
+  );
+}
+
+export default App;
---
+++ src/component/Presentacion.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+import './Styles.css'
+export const Presentacion = () => {
+  return (
+    
+    <div className="container">
+    <div className="header">
+        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*rr8oPcH3uzrw6DP7" alt="Foto de perfil"/>
+        <h1>Josue Guzman</h1>
+        <p>Developer</p>
+    </div>
+    </div>
+    );
+};
